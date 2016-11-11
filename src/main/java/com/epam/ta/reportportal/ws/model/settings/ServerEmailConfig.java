d1f477ba81dce06cde3aa496fd1ddfc2ee056f89
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-11-11 18:38:34 +0300

    update email server config. Add possibility to configure starttls

--- src/main/java/com/epam/ta/reportportal/ws/model/settings/ServerEmailConfig.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/settings/ServerEmailConfig.java
@@ -55,6 +55,9 @@ public class ServerEmailConfig implements Serializable {
 	@JsonProperty(value = "authEnabled")
 	private Boolean authEnabled = false;
 
+	@JsonProperty
+	private Boolean startTlsEnabled = false;
+
 	@JsonProperty(value = "username")
 	private String username;
 
@@ -68,12 +71,13 @@ public class ServerEmailConfig implements Serializable {
 	public ServerEmailConfig() {
 	}
 
-	public ServerEmailConfig(final String host, final int port, final String protocol, final boolean isAuth, final String username,
-			final String password, final boolean debug) {
+	public ServerEmailConfig(String host, int port, String protocol, Boolean authEnabled, Boolean startTlsEnabled,
+			String username, String password, boolean debug) {
 		this.host = host;
 		this.port = port;
-		this.protocol = protocol.toLowerCase();
-		this.authEnabled = isAuth;
+		this.protocol = protocol;
+		this.authEnabled = authEnabled;
+		this.startTlsEnabled = startTlsEnabled;
 		this.username = username;
 		this.password = password;
 		this.debug = debug;
@@ -104,14 +108,6 @@ public class ServerEmailConfig implements Serializable {
 		return protocol;
 	}
 
-	public void setAuthEnabled(boolean enable) {
-		this.authEnabled = enable;
-	}
-
-	public Boolean getAuthEnabled() {
-		return authEnabled;
-	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
@@ -126,6 +122,18 @@ public class ServerEmailConfig implements Serializable {
 
 	public String getPassword() {
 		return password;
+	}
+
+	public void setAuthEnabled(Boolean authEnabled) {
+		this.authEnabled = authEnabled;
+	}
+
+	public Boolean getStartTlsEnabled() {
+		return startTlsEnabled;
+	}
+
+	public void setStartTlsEnabled(Boolean startTlsEnabled) {
+		this.startTlsEnabled = startTlsEnabled;
 	}
 
 	public void setDebug(boolean debug) {
