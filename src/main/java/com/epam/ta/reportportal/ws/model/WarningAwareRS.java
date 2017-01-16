abad6f2a1d5c7bc52328768988cab3d73660ee36
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-01-16 14:19:07 +0300

    add warning aware RS model

--- src/main/java/com/epam/ta/reportportal/ws/model/WarningAwareRS.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/WarningAwareRS.java
@@ -1,55 +1,44 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
- 
-package com.epam.ta.reportportal.ws.model.user;
+ */
+package com.epam.ta.reportportal.ws.model;
 
-import com.epam.ta.reportportal.ws.model.WarningAwareRS;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.google.common.base.MoreObjects;
 
 /**
- * 
- * @author Aliaksandr_Kazantsau
- * 
+ * @author Andrei Varabyeu
  */
-@JsonInclude(Include.NON_NULL)
-public class CreateUserRS extends WarningAwareRS {
+public class WarningAwareRS {
 
-	@JsonProperty("login")
-	private String login;
+	private String warning;
 
-	public String getLogin() {
-		return login;
+	public String getWarning() {
+		return warning;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public void setWarning(String warning) {
+		this.warning = warning;
 	}
 
 	@Override
 	public String toString() {
-		final StringBuilder sb = new StringBuilder("CreateUserRS{");
-		sb.append("login='").append(login).append('\'');
-		sb.append('}');
-		return sb.toString();
+		return MoreObjects.toStringHelper(this).add("warning", warning).toString();
 	}
 }
