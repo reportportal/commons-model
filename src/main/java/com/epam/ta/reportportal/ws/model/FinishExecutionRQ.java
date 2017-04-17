9c77dbb3f4dad48ff9b8d2879fbba07355d36606
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-04-17 19:49:53 +0300

    Updated some swagger documentation

--- src/main/java/com/epam/ta/reportportal/ws/model/FinishExecutionRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/FinishExecutionRQ.java
@@ -21,14 +21,13 @@
  
 package com.epam.ta.reportportal.ws.model;
 
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * Finishes some entity execution in Report Portal<br>
@@ -46,6 +45,7 @@ public class FinishExecutionRQ {
 	private Date endTime;
 
 	@JsonProperty(value = "status")
+	@ApiModelProperty(allowableValues = "passed, failed, stopped, skipped, failed, rested, cancelled")
 	private String status;
 
 	public Date getEndTime() {
--- src/main/java/com/epam/ta/reportportal/ws/model/log/SaveLogRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/log/SaveLogRQ.java
@@ -21,16 +21,15 @@
  
 package com.epam.ta.reportportal.ws.model.log;
 
-import java.util.Arrays;
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * 
@@ -55,6 +54,7 @@ public class SaveLogRQ {
 	private String message;
 	
 	@JsonProperty(value = "level")
+	@ApiModelProperty(allowableValues = "error, warn, info, debug, trace, fatal, unknown")
 	private String level;
 	
 	@JsonProperty(value = "file")
