6794842095c021cf26e0494563508bc5f331e0f5
avarabyeu <andrei.varabyeu@gmail.com>
2017-10-25 12:22:42 +0300

    update request
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
@@ -60,15 +60,15 @@ public class StartTestItemRQ extends StartRQ {
 			+ "BEFORE_METHOD, BEFORE_SUITE, BEFORE_TEST, AFTER_CLASS, AFTER_GROUPS, AFTER_METHOD, AFTER_SUITE, AFTER_TEST")
 	private String type;
 
-	@JsonProperty(value = "rerun")
-	private boolean rerun;
+	@JsonProperty(value = "retry")
+	private boolean retry;
 
-	public boolean getRerun() {
-		return rerun;
+	public boolean isRetry() {
+		return retry;
 	}
 
-	public void setRerun(boolean rerun) {
-		this.rerun = rerun;
+	public void setRetry(boolean retry) {
+		this.retry = retry;
 	}
 
 	public String getLaunchId() {
@@ -132,7 +132,7 @@ public class StartTestItemRQ extends StartRQ {
 		sb.append(", uniqueId='").append(uniqueId).append('\'');
 		sb.append(", launchId='").append(launchId).append('\'');
 		sb.append(", type='").append(type).append('\'');
-		sb.append(", rerun=").append(rerun);
+		sb.append(", retry=").append(retry);
 		sb.append('}');
 		return sb.toString();
 	}
