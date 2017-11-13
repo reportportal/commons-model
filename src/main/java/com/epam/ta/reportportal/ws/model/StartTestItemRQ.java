4832ee16b285c04e7f5fc8b1b223134260269bdd
avarabyeu <andrei.varabyeu@gmail.com>
2017-11-13 13:35:37 +0300

    revert back retry field
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
@@ -60,6 +60,17 @@ public class StartTestItemRQ extends StartRQ {
 			+ "BEFORE_METHOD, BEFORE_SUITE, BEFORE_TEST, AFTER_CLASS, AFTER_GROUPS, AFTER_METHOD, AFTER_SUITE, AFTER_TEST")
 	private String type;
 
+	@JsonProperty(value = "retry")
+	private boolean retry;
+
+	public boolean isRetry() {
+		return retry;
+	}
+
+	public void setRetry(boolean retry) {
+		this.retry = retry;
+	}
+
 	public String getLaunchId() {
 		return launchId;
 	}
@@ -121,6 +132,7 @@ public class StartTestItemRQ extends StartRQ {
 		sb.append(", uniqueId='").append(uniqueId).append('\'');
 		sb.append(", launchId='").append(launchId).append('\'');
 		sb.append(", type='").append(type).append('\'');
+		sb.append(", retry=").append(retry);
 		sb.append('}');
 		return sb.toString();
 	}
