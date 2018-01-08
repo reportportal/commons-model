5cf3ed9e4685bf6ef854d1be3824b1e3a1be048c
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-01-08 16:27:02 +0300

    fix tests

--- src/test/java/com/epam/ta/reportportal/ws/model/FinishTestItemSerializerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/model/FinishTestItemSerializerTest.java
@@ -1,26 +1,27 @@
 /*
- * Copyright 2016 EPAM Systems
- * 
- * 
+ * Copyright 2018 EPAM Systems
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.epam.ta.reportportal.ws.model;
 
+import com.epam.ta.reportportal.ws.model.issue.Issue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -28,38 +29,39 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
- * Test Item custom serializer test
- *
- * @author Andrei Varabyeu
+ * @author Pavel Bortnik
  */
-public class StartTestItemSerializerTest {
+public class FinishTestItemSerializerTest {
 
 	private ObjectMapper om = getObjectMapper();
 
-	private static final String START_ITEM_RQ = "{\"description\":\"description\",\"launch_id\":\"some id\",\"type\":\"LAUNCH\",\"retry\":false}";
+	private static final String FINISH_TEST_ITEM_RQ = "{\"tags\":[],\"status\":\"PASSED\",\"description\":\"description\","
+			+ "\"issue\":{\"autoAnalyzed\":false,\"ignoreAnalyzer\":false},\"retry\":false}";
 
 	@Test
 	public void testSerializer() throws JsonProcessingException {
-		String json = om.writeValueAsString(getStartTestItem());
-		Assert.assertEquals("Incorrect serialization result", START_ITEM_RQ, json);
+		String json = om.writeValueAsString(getFinishTestItem());
+		Assert.assertEquals("Incorrect serialization result", FINISH_TEST_ITEM_RQ, json);
 	}
 
 	@Test
 	public void testDeserializer() throws IOException {
-		StartTestItemRQ rq = om.readValue(START_ITEM_RQ.toLowerCase().getBytes(), StartTestItemRQ.class);
-		Assert.assertEquals("Incorrect deserialization result", rq.getType(), "LAUNCH");
+		FinishTestItemRQ rq = om.readValue(FINISH_TEST_ITEM_RQ.getBytes(), FinishTestItemRQ.class);
+		Assert.assertEquals("Incorrect deserialization result", rq.getStatus(), "PASSED");
 	}
 
-	private StartTestItemRQ getStartTestItem() {
-		StartTestItemRQ startTestItem = new StartTestItemRQ();
-		startTestItem.setDescription("description");
-		startTestItem.setLaunchId("some id");
-		startTestItem.setType("launch");
-		startTestItem.setRetry(false);
+	private FinishTestItemRQ getFinishTestItem() {
+		FinishTestItemRQ finishTestItemRQ = new FinishTestItemRQ();
+		finishTestItemRQ.setStatus("PASSED");
+		finishTestItemRQ.setRetry(false);
+		finishTestItemRQ.setDescription("description");
+		finishTestItemRQ.setIssue(new Issue());
+		finishTestItemRQ.setTags(Collections.<String>emptySet());
 
-		return startTestItem;
+		return finishTestItemRQ;
 	}
 
 	private ObjectMapper getObjectMapper() {
@@ -67,4 +69,5 @@ public class StartTestItemSerializerTest {
 		om.configure(SerializationFeature.INDENT_OUTPUT, false);
 		return om;
 	}
+
 }
