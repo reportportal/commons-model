f7764cc74bd54c2367910a4c59957d094a172421
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-04-05 17:28:41 +0300

    Introduce unlink issue rq

---
+++ src/main/java/com/epam/ta/reportportal/ws/model/item/UnlinkExternalIssueRq.java
@@ -0,0 +1,50 @@
+package com.epam.ta.reportportal.ws.model.item;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author Pavel Bortnik
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UnlinkExternalIssueRq {
+
+	@NotNull
+	@JsonProperty(value = "systemId")
+	private Long externalSystemId;
+
+	@NotNull
+	@JsonProperty(value = "testItemIds")
+	private List<Long> testItemIds;
+
+	@NotNull
+	@JsonProperty(value = "issue_ids")
+	private List<String> issueIds;
+
+	public Long getExternalSystemId() {
+		return externalSystemId;
+	}
+
+	public void setExternalSystemId(Long externalSystemId) {
+		this.externalSystemId = externalSystemId;
+	}
+
+	public List<Long> getTestItemIds() {
+		return testItemIds;
+	}
+
+	public void setTestItemIds(List<Long> testItemIds) {
+		this.testItemIds = testItemIds;
+	}
+
+	public List<String> getIssueIds() {
+		return issueIds;
+	}
+
+	public void setIssueIds(List<String> issueIds) {
+		this.issueIds = issueIds;
+	}
+}
