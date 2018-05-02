8058233c22bb48e03d3df9a1c7b85c9f14679592
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-05-02 17:59:11 +0300

    remove unused fields

--- src/main/java/com/epam/ta/reportportal/ws/model/BasicReportingRq.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/BasicReportingRq.java
@@ -3,8 +3,6 @@ package com.epam.ta.reportportal.ws.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Map;
-
 /**
  * @author Pavel Bortnik
  */
@@ -16,12 +14,6 @@ public class BasicReportingRq {
 
 	@JsonProperty("username")
 	private String username;
-
-	@JsonProperty("role")
-	private String role;
-
-	@JsonProperty("projectDetails")
-	private Map<String, ProjectDetails> projectDetails;
 
 	public String getProjectName() {
 		return projectName;
@@ -37,46 +29,5 @@ public class BasicReportingRq {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public Map<String, ProjectDetails> getProjectDetails() {
-		return projectDetails;
-	}
-
-	public void setProjectDetails(Map<String, ProjectDetails> projectDetails) {
-		this.projectDetails = projectDetails;
-	}
-
-	public static class ProjectDetails {
-
-		@JsonProperty("projectId")
-		private Long projectId;
-
-		@JsonProperty("projectRole")
-		private String projectRole;
-
-		public Long getProjectId() {
-			return projectId;
-		}
-
-		public void setProjectId(Long projectId) {
-			this.projectId = projectId;
-		}
-
-		public String getProjectRole() {
-			return projectRole;
-		}
-
-		public void setProjectRole(String projectRole) {
-			this.projectRole = projectRole;
-		}
 	}
 }
