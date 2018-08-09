1a2406a2bea32322a505043aab851faee3dca32e
pbortnik <rrrizon@gmail.com>
2018-08-09 16:32:05 +0300

    fix ids

--- src/main/java/com/epam/ta/reportportal/ws/model/TestItemResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/TestItemResource.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -35,10 +35,8 @@ import java.util.Set;
 /**
  * JSON Representation of Report Portal domain object
  *
- * @see <a href="http://en.wikipedia.org/wiki/HATEOAS">HATEOAS Description</a>
- *
  * @author Andrei Varabyeu
- *
+ * @see <a href="http://en.wikipedia.org/wiki/HATEOAS">HATEOAS Description</a>
  */
 @JsonInclude(Include.NON_NULL)
 public class TestItemResource {
@@ -74,7 +72,7 @@ public class TestItemResource {
 	private Statistics statistics;
 
 	@JsonProperty(value = "parent")
-	private String parent;
+	private Long parent;
 
 	@JsonProperty(value = "path_names")
 	private Map<String, String> pathNames;
@@ -89,7 +87,7 @@ public class TestItemResource {
 	private boolean hasChilds;
 
 	@JsonProperty(value = "launchId")
-	private String launchId;
+	private Long launchId;
 
 	@JsonProperty(value = "uniqueId")
 	private String uniqueId;
@@ -105,11 +103,11 @@ public class TestItemResource {
 		this.retries = retries;
 	}
 
-	public String getLaunchId() {
+	public Long getLaunchId() {
 		return launchId;
 	}
 
-	public void setLaunchId(String launchId) {
+	public void setLaunchId(Long launchId) {
 		this.launchId = launchId;
 	}
 
@@ -193,11 +191,11 @@ public class TestItemResource {
 		this.status = status;
 	}
 
-	public String getParent() {
+	public Long getParent() {
 		return parent;
 	}
 
-	public void setParent(String parent) {
+	public void setParent(Long parent) {
 		this.parent = parent;
 	}
 
@@ -243,24 +241,10 @@ public class TestItemResource {
 
 	@Override
 	public String toString() {
-		return "TestItemResource{" +
-				"itemId='" + itemId + '\'' +
-				", name='" + name + '\'' +
-				", description='" + description + '\'' +
-				", parameters='" + parameters + '\'' +
-				", tags=" + tags +
-				", type='" + type + '\'' +
-				", startTime=" + startTime +
-				", endTime=" + endTime +
-				", status='" + status + '\'' +
-				", statistics=" + statistics +
-				", parent='" + parent + '\'' +
-				", pathNames=" + pathNames +
-				", launchStatus='" + launchStatus + '\'' +
-				", issue=" + issue +
-				", hasChilds=" + hasChilds +
-				", launchId='" + launchId + '\'' +
-				", uniqueId='" + uniqueId + '\'' +
-				'}';
+		return "TestItemResource{" + "itemId='" + itemId + '\'' + ", name='" + name + '\'' + ", description='" + description + '\''
+				+ ", parameters='" + parameters + '\'' + ", tags=" + tags + ", type='" + type + '\'' + ", startTime=" + startTime
+				+ ", endTime=" + endTime + ", status='" + status + '\'' + ", statistics=" + statistics + ", parent='" + parent + '\''
+				+ ", pathNames=" + pathNames + ", launchStatus='" + launchStatus + '\'' + ", issue=" + issue + ", hasChilds=" + hasChilds
+				+ ", launchId='" + launchId + '\'' + ", uniqueId='" + uniqueId + '\'' + '}';
 	}
 }
