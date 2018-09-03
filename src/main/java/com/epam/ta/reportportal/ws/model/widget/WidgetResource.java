dc8a02aa340460f2b0c92eb1287b0bc72713684a
yumfriez <budaevqwerty@gmail.com>
2018-09-03 11:53:40 +0300

    WidgetResource structure was changed according to the new structure

--- src/main/java/com/epam/ta/reportportal/ws/model/widget/WidgetResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/widget/WidgetResource.java
@@ -30,6 +30,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -52,8 +53,8 @@ public class WidgetResource extends OwnedResource {
 	@JsonProperty(value = "content_parameters", required = true)
 	private ContentParameters contentParameters;
 
-	@JsonProperty(value = "applied_filter")
-	private UserFilterResource appliedFilters;
+	@JsonProperty(value = "applied_filters")
+	private List<UserFilterResource> appliedFilters;
 
 	@JsonProperty(value = "content")
 	private Map<String, ?> content;
@@ -74,11 +75,11 @@ public class WidgetResource extends OwnedResource {
 		this.name = name;
 	}
 
-	public UserFilterResource getAppliedFilters() {
+	public List<UserFilterResource> getAppliedFilters() {
 		return appliedFilters;
 	}
 
-	public void setAppliedFilters(UserFilterResource appliedFilters) {
+	public void setAppliedFilters(List<UserFilterResource> appliedFilters) {
 		this.appliedFilters = appliedFilters;
 	}
 
