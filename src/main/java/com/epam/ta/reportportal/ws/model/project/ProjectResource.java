236041ed0fa9377c235a2baac9c54815ace5b9d3
pbortnik <rrrizon@gmail.com>
2018-10-10 21:11:46 +0300

    update classes according to project model

--- src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
@@ -21,13 +21,11 @@
 
 package com.epam.ta.reportportal.ws.model.project;
 
-import com.epam.ta.reportportal.ws.model.ValidationConstraints;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
@@ -41,15 +39,11 @@ public class ProjectResource {
 
 	@NotNull
 	@JsonProperty(value = "projectId", required = true)
-	private String projectId;
+	private Long projectId;
 
 	@NotNull
-	@JsonProperty(value = "customer")
-	@Size(max = ValidationConstraints.MAX_CUSTOMER_LENGTH)
-	private String customer;
-
-	@JsonProperty(value = "addInfo")
-	private String addInfo;
+	@JsonProperty(value = "projectName", required = true)
+	private String projectName;
 
 	@NotNull
 	@JsonProperty(value = "configuration", required = true)
@@ -70,28 +64,20 @@ public class ProjectResource {
 		this.creationDate = creationDate;
 	}
 
-	public void setProjectId(String id) {
-		this.projectId = id;
-	}
-
-	public String getProjectId() {
+	public Long getProjectId() {
 		return projectId;
 	}
 
-	public void setCustomer(String customer) {
-		this.customer = customer;
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
 	}
 
-	public String getCustomer() {
-		return customer;
+	public String getProjectName() {
+		return projectName;
 	}
 
-	public void setAddInfo(String additional) {
-		this.addInfo = additional;
-	}
-
-	public String getAddInfo() {
-		return addInfo;
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
 	}
 
 	public void setConfiguration(ProjectConfiguration configuration) {
