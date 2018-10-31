5e757146cb4869f191948f544a94673afa1e95a9
pbortnik <pavel_bortnik@epam.com>
2018-10-31 13:58:52 +0300

    update preferences rq

--- src/main/java/com/epam/ta/reportportal/ws/model/preference/UpdatePreferenceRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/preference/UpdatePreferenceRQ.java
@@ -1,35 +1,35 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
- 
-package com.epam.ta.reportportal.ws.model.preference;
+ */
 
-import java.util.List;
+package com.epam.ta.reportportal.ws.model.preference;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 /**
  * Domain object for updating user preference
- * 
+ *
  * @author Dzmitry_Kavalets
  */
 
@@ -37,33 +37,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class UpdatePreferenceRQ {
 
 	@JsonProperty(value = "filters")
-	private List<String> filters;
+	private List<Long> filters;
 
-	@JsonProperty(value = "active")
-	private String active;
-
-	public List<String> getFilters() {
+	public List<Long> getFilters() {
 		return filters;
 	}
 
-	public void setFilters(List<String> filters) {
+	public void setFilters(List<Long> filters) {
 		this.filters = filters;
-	}
-
-	public String getActive() {
-		return active;
-	}
-
-	public void setActive(String active) {
-		this.active = active;
-	}
-
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("UpdatePreferenceRQ{");
-		sb.append("filters=").append(filters);
-		sb.append(", active='").append(active).append('\'');
-		sb.append('}');
-		return sb.toString();
 	}
 }
