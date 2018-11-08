f43ba7f95bbe9e5d560b5c7ffca382d481eb5985
pavel_bortnik <pavel_bortnik@epam.com>
2018-11-08 17:25:20 +0300

    introduce email as an integration

--- src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
@@ -1,26 +1,23 @@
 /*
- * Copyright 2016 EPAM Systems
+ * Copyright 2018 EPAM Systems
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file is part of EPAM Report Portal.
- * https://github.com/reportportal/commons-model
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Report Portal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Report Portal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.epam.ta.reportportal.ws.model.project;
 
+import com.epam.ta.reportportal.ws.model.integration.IntegrationResource;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -32,7 +29,7 @@ import java.util.List;
 /**
  * Project resource representation for responses
  *
- * @author Andrei_Ramanchuk
+ * @author Pavel Bortnik
  */
 @JsonInclude(Include.NON_NULL)
 public class ProjectResource {
@@ -51,6 +48,9 @@ public class ProjectResource {
 
 	@JsonProperty(value = "users")
 	private List<ProjectUser> users;
+
+	@JsonProperty(value = "integrations")
+	private List<IntegrationResource> integrations;
 
 	@NotNull
 	@JsonProperty(value = "creationDate")
@@ -86,6 +86,14 @@ public class ProjectResource {
 
 	public ProjectConfiguration getConfiguration() {
 		return configuration;
+	}
+
+	public List<IntegrationResource> getIntegrations() {
+		return integrations;
+	}
+
+	public void setIntegrations(List<IntegrationResource> integrations) {
+		this.integrations = integrations;
 	}
 
 	public List<ProjectUser> getUsers() {
