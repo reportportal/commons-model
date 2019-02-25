9074204bd41a64e073af6b849c5e250c419456c4
yumfriez <budaevqwerty@gmail.com>
2019-02-25 13:43:48 +0300

    Update Plugin state entity refactoring

--- src/main/java/com/epam/ta/reportportal/ws/model/integration/UpdatePluginStateRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/integration/UpdatePluginStateRQ.java
@@ -1,6 +1,5 @@
 package com.epam.ta.reportportal.ws.model.integration;
 
-import com.epam.ta.reportportal.ws.annotations.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.validation.constraints.NotNull;
@@ -13,21 +12,9 @@ import java.io.Serializable;
 public class UpdatePluginStateRQ implements Serializable {
 
 	@NotNull
-	@NotEmpty
-	private String name;
-
-	@NotNull
 	private Boolean isEnabled;
 
 	public UpdatePluginStateRQ() {
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public Boolean getEnabled() {
@@ -49,16 +36,11 @@ public class UpdatePluginStateRQ implements Serializable {
 
 		UpdatePluginStateRQ that = (UpdatePluginStateRQ) o;
 
-		if (name != null ? !name.equals(that.name) : that.name != null) {
-			return false;
-		}
 		return isEnabled != null ? isEnabled.equals(that.isEnabled) : that.isEnabled == null;
 	}
 
 	@Override
 	public int hashCode() {
-		int result = name != null ? name.hashCode() : 0;
-		result = 31 * result + (isEnabled != null ? isEnabled.hashCode() : 0);
-		return result;
+		return isEnabled != null ? isEnabled.hashCode() : 0;
 	}
 }
