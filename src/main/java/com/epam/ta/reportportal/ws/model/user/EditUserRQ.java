bb3872d7dd840ed4fc986b0aff2c6860bab9374c
yumfriez <budaevqwerty@gmail.com>
2019-03-07 12:56:55 +0300

    Default project has been removed from edit user request

--- src/main/java/com/epam/ta/reportportal/ws/model/user/EditUserRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/user/EditUserRQ.java
@@ -1,23 +1,23 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 
 package com.epam.ta.reportportal.ws.model.user;
 
@@ -32,10 +32,9 @@ import javax.validation.constraints.Size;
 
 /**
  * Edit User request model
- * 
+ *
  * @author Aliaksandr_Kazantsau
  * @author Andrei_Ramanchuk
- * 
  */
 @JsonInclude(Include.NON_NULL)
 public class EditUserRQ {
@@ -46,9 +45,6 @@ public class EditUserRQ {
 
 	@JsonProperty(value = "role")
 	private String role;
-
-	@JsonProperty(value = "defaultProject")
-	private String defaultProject;
 
 	@NotEmpty
 	@Size(min = ValidationConstraints.MIN_USER_NAME_LENGTH, max = ValidationConstraints.MAX_USER_NAME_LENGTH)
@@ -80,20 +76,11 @@ public class EditUserRQ {
 		return fullName;
 	}
 
-	public void setDefaultProject(String value) {
-		this.defaultProject = value;
-	}
-
-	public String getDefaultProject() {
-		return defaultProject;
-	}
-
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("EditUserRQ{");
 		sb.append("email='").append(email).append('\'');
 		sb.append(", role='").append(role).append('\'');
-		sb.append(", defaultProject='").append(defaultProject).append('\'');
 		sb.append(", fullName='").append(fullName).append('\'');
 		sb.append('}');
 		return sb.toString();
