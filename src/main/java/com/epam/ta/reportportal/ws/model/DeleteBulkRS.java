38eb1bd9049206c78eee6537c212894de7afe418
pavel_bortnik <pavel_bortnik@epam.com>
2019-03-11 17:04:15 +0300

    bulk delete rs object

--- src/main/java/com/epam/ta/reportportal/ws/model/DeleteBulkRS.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/DeleteBulkRS.java
@@ -5,18 +5,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package com.epam.ta.reportportal.ws.model.launch;
+package com.epam.ta.reportportal.ws.model;
 
-import com.epam.ta.reportportal.ws.model.ErrorRS;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
@@ -24,7 +24,7 @@ import java.util.List;
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
-public class DeleteLaunchesRS {
+public class DeleteBulkRS {
 
 	@JsonProperty("successfullyDeleted")
 	private List<Long> deleted;
@@ -35,10 +35,10 @@ public class DeleteLaunchesRS {
 	@JsonProperty("errors")
 	private List<ErrorRS> errors;
 
-	public DeleteLaunchesRS() {
+	public DeleteBulkRS() {
 	}
 
-	public DeleteLaunchesRS(List<Long> deleted, List<Long> notFound, List<ErrorRS> errors) {
+	public DeleteBulkRS(List<Long> deleted, List<Long> notFound, List<ErrorRS> errors) {
 		this.deleted = deleted;
 		this.notFound = notFound;
 		this.errors = errors;
