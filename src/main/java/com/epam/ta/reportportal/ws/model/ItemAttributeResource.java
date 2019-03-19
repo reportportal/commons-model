e7ced20c6cc2e5da7af777d39266379f0598f185
yumfriez <budaevqwerty@gmail.com>
2019-03-19 14:06:35 +0300

    Serializable impl statement has been added

--- src/main/java/com/epam/ta/reportportal/ws/model/ItemAttributeResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/ItemAttributeResource.java
@@ -22,13 +22,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import java.io.Serializable;
+
 import static com.epam.ta.reportportal.ws.model.ValidationConstraints.MAX_ATTRIBUTE_LENGTH;
 import static com.epam.ta.reportportal.ws.model.ValidationConstraints.MIN_ITEM_ATTRIBUTE_VALUE_LENGTH;
 
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
-public class ItemAttributeResource {
+public class ItemAttributeResource implements Serializable {
 
 	@Size(max = MAX_ATTRIBUTE_LENGTH)
 	private String key;
