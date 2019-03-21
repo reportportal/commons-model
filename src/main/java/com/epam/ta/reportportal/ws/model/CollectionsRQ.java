62d62882708655c4e91d4d30eb885c1e22b26b06
pavel_bortnik <pavel_bortnik@epam.com>
2019-03-21 13:49:49 +0300

    return bulk rq objects

---
+++ src/main/java/com/epam/ta/reportportal/ws/model/CollectionsRQ.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.epam.ta.reportportal.ws.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @deprecated use {@link BulkRQ} instead
+ */
+@Deprecated
+public class CollectionsRQ<T> {
+
+	@Valid
+	@NotNull
+	@JsonProperty(value = "elements", required = true)
+	private List<T> elements;
+
+	public List<T> getElements() {
+		return elements;
+	}
+
+	public void setElements(List<T> elements) {
+		this.elements = elements;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("CollectionsRQ{");
+		sb.append("elements=").append(elements);
+		sb.append('}');
+		return sb.toString();
+	}
+}
