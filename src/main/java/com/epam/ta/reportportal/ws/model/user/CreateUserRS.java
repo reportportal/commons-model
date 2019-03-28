913b4a04e14e91ea4332eecf714f62016815da9c
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-03-28 13:02:08 +0300

    Add login to create user response

--- src/main/java/com/epam/ta/reportportal/ws/model/user/CreateUserRS.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/user/CreateUserRS.java
@@ -1,41 +1,50 @@
 /*
- * Copyright 2016 EPAM Systems
- * 
- * 
- * This file is part of EPAM Report Portal.
- * https://github.com/reportportal/commons-model
- * 
- * Report Portal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Report Portal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
- 
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.ws.model.user;
 
 import com.epam.ta.reportportal.ws.model.WarningAwareRS;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * 
  * @author Aliaksandr_Kazantsau
- * 
  */
 @JsonInclude(Include.NON_NULL)
 public class CreateUserRS extends WarningAwareRS {
 
+	@JsonProperty("id")
+	private Long id;
+
 	@JsonProperty("login")
 	private String login;
+
+	public CreateUserRS(Long id, String login) {
+		this.id = id;
+		this.login = login;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getLogin() {
 		return login;
@@ -48,7 +57,8 @@ public class CreateUserRS extends WarningAwareRS {
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("CreateUserRS{");
-		sb.append("login='").append(login).append('\'');
+		sb.append("id=").append(id);
+		sb.append(", login='").append(login).append('\'');
 		sb.append('}');
 		return sb.toString();
 	}
