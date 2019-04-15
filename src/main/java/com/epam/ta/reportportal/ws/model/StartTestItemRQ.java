34cda0ff3e3c77f581567c2032472d410c05cb63
yumfriez <budaevqwerty@gmail.com>
2019-04-15 12:37:20 +0300

    Has stats flag has been provided

--- src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
@@ -1,24 +1,24 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-model
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
- 
+ */
+
 package com.epam.ta.reportportal.ws.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -63,12 +63,23 @@ public class StartTestItemRQ extends StartRQ {
 	@JsonProperty(value = "retry")
 	private Boolean retry;
 
+	@JsonProperty(value = "hasStats")
+	private boolean hasStats = true;
+
 	public Boolean isRetry() {
 		return retry;
 	}
 
 	public void setRetry(Boolean retry) {
 		this.retry = retry;
+	}
+
+	public boolean isHasStats() {
+		return hasStats;
+	}
+
+	public void setHasStats(boolean hasStats) {
+		this.hasStats = hasStats;
 	}
 
 	public Long getLaunchId() {
