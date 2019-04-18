1ba3937570d4889c8d4fee4a123224fb7ebc5f20
pavel_bortnik <pavel_bortnik@epam.com>
2019-04-18 13:23:14 +0300

    devide responce system and rq for item attributes

--- src/main/java/com/epam/ta/reportportal/ws/model/StartRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/StartRQ.java
@@ -1,17 +1,18 @@
 /*
- * Copyright 2019 EPAM Systems
+ * Copyright 2018 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.epam.ta.reportportal.ws.model;
@@ -51,7 +52,7 @@ public class StartRQ {
 	@Size(max = MAX_PARAMETERS_LENGTH)
 	@Valid
 	@JsonProperty("attributes")
-	private Set<ItemAttributeResource> attributes;
+	private Set<ItemAttributesRQ> attributes;
 
 	@NotNull
 	@JsonProperty(value = "startTime", required = true)
@@ -78,11 +79,11 @@ public class StartRQ {
 		this.description = description;
 	}
 
-	public Set<ItemAttributeResource> getAttributes() {
+	public Set<ItemAttributesRQ> getAttributes() {
 		return attributes;
 	}
 
-	public void setAttributes(Set<ItemAttributeResource> attributes) {
+	public void setAttributes(Set<ItemAttributesRQ> attributes) {
 		this.attributes = attributes;
 	}
 
