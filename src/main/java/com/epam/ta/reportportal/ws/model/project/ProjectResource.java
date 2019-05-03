f00b25059d5420b22602e1f72422ee87c4c9762c
yumfriez <budaevqwerty@gmail.com>
2019-05-03 15:55:22 +0300

    Pattern template resource has been added to the Project resource

--- src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/project/ProjectResource.java
@@ -18,6 +18,7 @@
 package com.epam.ta.reportportal.ws.model.project;
 
 import com.epam.ta.reportportal.ws.model.integration.IntegrationResource;
+import com.epam.ta.reportportal.ws.model.project.config.pattern.PatternTemplateResource;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotNull;
@@ -55,6 +56,9 @@ public class ProjectResource {
 	@NotNull
 	@JsonProperty(value = "creationDate")
 	private Date creationDate;
+
+	@JsonProperty(value = "patterns")
+	private List<PatternTemplateResource> patterns;
 
 	public Date getCreationDate() {
 		return creationDate;
@@ -110,6 +114,14 @@ public class ProjectResource {
 
 	public void setOrganization(String organization) {
 		this.organization = organization;
+	}
+
+	public List<PatternTemplateResource> getPatterns() {
+		return patterns;
+	}
+
+	public void setPatterns(List<PatternTemplateResource> patterns) {
+		this.patterns = patterns;
 	}
 
 	public static class ProjectUser {
