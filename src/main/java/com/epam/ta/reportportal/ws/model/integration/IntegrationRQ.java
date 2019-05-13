438a6c1e56da3ebaa1aa3d4dd9064e4992b30311
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-05-13 19:31:51 +0300

    Merge branch 'ps-migrations' of github.com:reportportal/commons-model into sende-case-validation
    # Conflicts:
    #	src/main/java/com/epam/ta/reportportal/ws/model/integration/CreateIntegrationRQ.java
    #	src/main/java/com/epam/ta/reportportal/ws/model/launch/LaunchResource.java
    
    refactoring

--- src/main/java/com/epam/ta/reportportal/ws/model/integration/IntegrationRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/integration/IntegrationRQ.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2018 EPAM Systems
+ * Copyright 2019 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.epam.ta.reportportal.ws.model.integration;
@@ -20,14 +19,13 @@ package com.epam.ta.reportportal.ws.model.integration;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UpdateIntegrationRQ {
+public class IntegrationRQ {
 
 	@JsonProperty("name")
 	private String name;
@@ -35,7 +33,6 @@ public class UpdateIntegrationRQ {
 	@JsonProperty("integrationParameters")
 	private Map<String, Object> integrationParams;
 
-	@NotNull
 	@JsonProperty("enabled")
 	private Boolean enabled;
 
