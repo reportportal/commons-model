e9b58ad6cd6699848f85d22850705c98aeb4f277
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-07-08 11:28:16 +0300

    Change pattern short name length validations

--- src/main/java/com/epam/ta/reportportal/ws/model/ValidationConstraints.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/ValidationConstraints.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.epam.ta.reportportal.ws.model;
@@ -88,8 +87,8 @@ public class ValidationConstraints {
 	public static final int MIN_SUBTYPE_LONG_NAME = 3;
 	public static final int MAX_SUBTYPE_LONG_NAME = 55;
 
-	public static final int MIN_ANALYSIS_PATTERN_NAME_LENGTH = 3;
-	public static final int MAX_ANALYSIS_PATTERN_NAME_LENGTH = 128;
+	public static final int MIN_ANALYSIS_PATTERN_NAME_LENGTH = 1;
+	public static final int MAX_ANALYSIS_PATTERN_NAME_LENGTH = 55;
 
 	public static final int MAX_DESCRIPTION = 256;
 	public static final int MIN_DESCRIPTION = 1;
