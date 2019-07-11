61f7a9dd244fd5ee0eef4c4435d07e971a101e96
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-07-11 12:05:09 +0300

    Base url has been added to finish execution request

--- src/main/java/com/epam/ta/reportportal/ws/model/FinishExecutionRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/FinishExecutionRQ.java
@@ -5,20 +5,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.epam.ta.reportportal.ws.model;
 
 import com.epam.ta.reportportal.ws.annotations.In;
 import com.epam.ta.reportportal.ws.model.attribute.ItemAttributesRQ;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -59,6 +59,9 @@ public class FinishExecutionRQ {
 	@JsonProperty
 	private Set<ItemAttributesRQ> attributes;
 
+	@JsonIgnore
+	private String baseURL;
+
 	public String getDescription() {
 		return description;
 	}
@@ -89,6 +92,14 @@ public class FinishExecutionRQ {
 
 	public void setStatus(String status) {
 		this.status = status;
+	}
+
+	public String getBaseURL() {
+		return baseURL;
+	}
+
+	public void setBaseURL(String baseURL) {
+		this.baseURL = baseURL;
 	}
 
 	@Override
