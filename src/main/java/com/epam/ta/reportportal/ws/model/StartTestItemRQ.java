09abeae41b7bdf69a836f11222223de211b1dcdf
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-08-07 17:57:39 +0300

    add descriptions and default values

--- src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/StartTestItemRQ.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.epam.ta.reportportal.ws.model;
@@ -29,6 +28,7 @@ import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -55,7 +55,7 @@ public class StartTestItemRQ extends StartRQ {
 
 	@NotBlank
 	@JsonProperty(value = "launchUuid", required = true)
-	@ApiModelProperty(required = true)
+	@ApiModelProperty(value = "UUID of parent launch", required = true)
 	private String launchUuid;
 
 	@NotNull
@@ -69,6 +69,7 @@ public class StartTestItemRQ extends StartRQ {
 	private String type;
 
 	@JsonProperty(value = "retry")
+	@ApiParam(defaultValue = "false")
 	private Boolean retry;
 
 	@JsonProperty(value = "hasStats")
--- src/main/java/com/epam/ta/reportportal/ws/model/launch/StartLaunchRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/launch/StartLaunchRQ.java
@@ -5,16 +5,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */ 
- 
+ */
+
 package com.epam.ta.reportportal.ws.model.launch;
 
 import com.epam.ta.reportportal.ws.annotations.NotBlankWithSize;
@@ -23,6 +22,8 @@ import com.epam.ta.reportportal.ws.model.ValidationConstraints;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 
 @JsonInclude(Include.NON_NULL)
 public class StartLaunchRQ extends StartRQ {
@@ -31,9 +32,11 @@ public class StartLaunchRQ extends StartRQ {
 	private Mode mode;
 
 	@JsonProperty("rerun")
+	@ApiParam(defaultValue = "false")
 	private boolean rerun;
 
 	@JsonProperty("rerunOf")
+	@ApiModelProperty(value = "UUID of desired launch to rerun")
 	private String rerunOf;
 
 	@Override
