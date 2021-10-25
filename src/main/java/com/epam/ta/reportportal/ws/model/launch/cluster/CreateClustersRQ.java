a669ad6ee2052b84f576a95ac7739ff3e5271796
Ivan_Budayeu <Skileton2018>
2021-10-25 18:25:50 +0300

    Launch id field added

--- src/main/java/com/epam/ta/reportportal/ws/model/launch/cluster/CreateClustersRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/model/launch/cluster/CreateClustersRQ.java
@@ -3,13 +3,27 @@ package com.epam.ta.reportportal.ws.model.launch.cluster;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateClustersRQ {
+
+	@NotNull
+	@JsonProperty(value = "launchId", required = true)
+	private Long launchId;
 
 	@JsonProperty(value = "removeNumbers")
 	private boolean removeNumbers;
 
 	public CreateClustersRQ() {
+	}
+
+	public Long getLaunchId() {
+		return launchId;
+	}
+
+	public void setLaunchId(Long launchId) {
+		this.launchId = launchId;
 	}
 
 	public boolean isRemoveNumbers() {
